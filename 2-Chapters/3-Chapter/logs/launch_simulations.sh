812653517f872210331355ceee61cf6ab2ba5112
Lilian Besson <lilian.besson@crans.org>
2019-05-07 20:06:55 +0200

    Longer simulations are in progress. The notebook to analyze simulations is nice

--- 2-Chapters/3-Chapter/logs/launch_simulations.sh
+++ 2-Chapters/3-Chapter/logs/launch_simulations.sh
@@ -11,11 +11,28 @@ nbexperiment=0
 
 for N in 100; do
 # for N in $N_JOBS; do
-    for BAYES in False True; do
-    # for BAYES in False; do
-        for T in 1000 2500 5000 10000 20000 25000 50000; do
-        # for T in 1000 2500; do
-            for K in 2 4 8 12 16 20 32; do
+    # for BAYES in False True; do
+    for BAYES in False; do
+        # for T in 1000 2500 5000 10000 20000 25000 50000; do
+        for T in 10000; do
+            # for K in 2 4 8 12 16 20 32; do
+            for K in 6 10 14 18 22 24 26 28 32; do
+            # for K in 2 4 8; do
+                debut=$(date)
+                nbexperiment+=1
+
+                DEBUG=$DEBUG NOPLOTS=$NOPLOTS SAVEALL=$SAVEALL N_JOBS=$N_JOBS \
+                N=$N T=$T K=$K BAYES=$BAYES mymake.sh --noFreeSMS single \
+                && cp -f logs/main_py3_log.txt \
+                            logs/main_py3_log__N${N}_BAYES${BAYES}_T${T}_K${K}__$(date +"%d-%m-%Y_%H-%M-%S").txt \
+                && FreeSMS.py "Expérience #${nbexperiment}, commencée ${debut}, terminée ! Avec N=${N}, BAYES=${BAYES}, T=${T}, K=${K}."
+            done
+        done
+        for T in 15000 30000 35000 40000 45000; do
+        # for T in 10000; do
+            # for K in 2 4 8 12 16 20 32; do
+            for K in 32; do
+            # for K in 6 10 14 18 22 24 26 28 32; do
             # for K in 2 4 8; do
                 debut=$(date)
                 nbexperiment+=1
