eabc40335d0414a16d2b2a5f1bbfc4401ec9222c
Lilian Besson <lilian.besson@crans.org>
2019-07-02 19:15:55 +0200

    Done for Chapters 2 and 3

--- 2-Chapters/3-Chapter/src/example_of_a_IndexPolicy_UCB.py
+++ 2-Chapters/3-Chapter/src/example_of_a_IndexPolicy_UCB.py
@@ -1,22 +1,19 @@
-from numpy import sqrt, log
-from .IndexPolicy import IndexPolicy
+from numpy import sqrt, log          # mathematical functions
+from IndexPolicy import IndexPolicy  # base class
 
 class UCB(IndexPolicy):
-  """ The UCB policy for bounded bandits.
-  Reference: [Lai & Robbins, 1985]. """
+    """ The UCB policy for bounded bandits.
+    Reference: [Lai & Robbins, 1985], [Auer et al. 2020]. """
 
-  def computeIndex(self, arm):
-    r""" Compute the current index, at time t and
-    after :math:`N_k(t)` pulls of arm k:
+    def computeIndex(self, arm):
+        r""" Compute the current index :math:`U_k(t)`, at time t (:attr:`self.t`)
+        and after :math:`N_k(t)` pulls of arm k (``arm``):
 
-    .. math::
-
-        I_k(t) = \frac{X_k(t)}{N_k(t)}
-        + \sqrt{\frac{2 \log(t)}{N_k(t)}}.
-    """
-    if self.pulls[arm] < 1:  # forced exploration
-      return float('+inf')   # in the first steps
-    else:                    # or compute UCB index
-      estimated_mean = (self.rewards[arm] / self.pulls[arm])
-      exploration_bias = sqrt((2 * log(self.t)) / self.pulls[arm])
-      return estimated_mean + exploration_bias
+        .. math:: U_k(t) = \frac{X_k(t)}{N_k(t)} + \sqrt{\frac{2 \log(t)}{N_k(t)}}.
+        """
+        if self.pulls[arm] < 1:    # forced exploration
+            return float('+inf')     # in the first steps
+        else:                      # or compute UCB index
+            estimated_mean = self.rewards[arm] / self.pulls[arm]
+            exploration_bias = sqrt((2 * log(self.t)) / self.pulls[arm])
+            return estimated_mean + exploration_bias
